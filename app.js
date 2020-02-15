490670bc28922c85a0e5f6502a20bf82376cc69c
Godwin Olletey <godwinolle@gmail.com>
2020-02-14 21:44:25 -0500

    Initial Commit of BMI Calculator

---
+++ app.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.bmi');
+const result = document.querySelector('div.result');
+
+const findBMI = (height, weight) =>{
+    let kg = weight/2.205;
+    let m = (height/39.37)**2;
+    return kg/m;
+}
+let bmiRange = "";
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    const user = form.name.value;
+    const age = form.age.value;
+    const height = form.height.value;
+    const weight = form.weight.value;
+
+    const bmi = findBMI(height, weight).toFixed(2);
+
+    if(bmi < 18.5){
+        bmiRange = "Underweight";
+    }
+    else if(bmi > 18.5 && bmi < 25){
+        bmiRange = "Normal";
+    }
+    else if(bmi > 25){
+        bmiRange = "Overweight";
+    }
+    else if(bmi > 30){
+        bmiRange = "Obese";
+    }
+
+    console.log(`BMI: ${bmi}`);
+
+    result.classList.remove('d-none');
+    result.textContent = `Hi ${user}, I see you're ${age} years old! `;
+    result.textContent+= `Your Body Mass Index: ${bmi} `;
+    result.textContent+= `BMI Range: ${bmiRange}`;
+});
